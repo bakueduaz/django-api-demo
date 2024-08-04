d9240a0e7edda865d6587b1d48e755078f067787
Nihad-NA <testaccount@gmail.com>
2024-08-04 16:37:03 +0400

    file updated

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import NewsView
 
 urlpatterns = [
-    path('api/news', NewsView.as_view(), name='news'),
+    path('api/news/', NewsView.as_view(), name='news'),
 ]
